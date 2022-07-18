8a232a85f7a3b321984a18e7a5e8cd7ac535d486
Aleksander Jaroshevich <jalxby@gmail.com>
2022-07-18 21:36:28 +0300

    React JS - 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
-            <Footer/>
-        </div>
-    );
+        <div className={'app-wrapper'}>
+        <Header/>
+        <Navbar/>
+        <Profile/>
+        </div>);
 }
-
-
-
 
 
 export default App;
